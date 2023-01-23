17a86038e59fa55c4d29038894371ac169ca7d69
Santhosh Saravana <109412677+SanthoshSaravana@users.noreply.github.com>
2023-01-23 16:47:25 +0530

    Add files via upload

---
+++ Alcohol_detection.ino
@@ -0,0 +1,29 @@
+int LED = 9;  
+    int BUZZER = 10;   
+    int ALCOHOL_sensor = 3;// MQ-3 SENSOR  
+    int ALCOHOL_detected;  
+    void setup()  
+    {  
+     Serial.begin(9600);  
+     pinMode(LED, OUTPUT);  
+     pinMode(BUZZER, OUTPUT);  
+     pinMode(ALCOHOL_sensor, INPUT);  
+    }  
+    void loop()  
+    {  
+      ALCOHOL_detected = digitalRead(ALCOHOL_sensor);  
+      Serial.println(ALCOHOL_detected);  
+     if (ALCOHOL_detected == 1) 
+     {  
+      Serial.println(" NO ALCOHOL detected...");  
+      digitalWrite(LED, LOW);  
+      digitalWrite(BUZZER, LOW);  
+     }  
+     else  
+     {  
+      Serial.println(" ALCOHOL detected.");  
+      digitalWrite(LED, HIGH);  
+      digitalWrite(BUZZER, HIGH);  
+     }  
+    } 
+    
